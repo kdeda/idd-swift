3f3cdd3e87eae648515f546f66bc061aaf78f237
Klajd Deda <kdeda@id-design.com>
2023-03-09 06:00:54 -0500

    Moving code from legacy idd-swift-commons

---
+++ Source/IDDSwift/DateFormatter.swift
@@ -0,0 +1,18 @@
+//
+//  DateFormatter.swift
+//  IDDSwift
+//
+//  Created by Klajd Deda on 11/4/17.
+//  Copyright (C) 1997-2023 id-design, inc. All rights reserved.
+//
+
+import Foundation
+
+extension DateFormatter {
+    convenience public init(withFormatString formatString: String, andPOSIXLocale posixLocale: Bool) {
+        self.init()
+        self.locale = Locale.init(identifier: "en_US_POSIX")
+        self.dateFormat = formatString
+        self.formatterBehavior = .behavior10_4
+    }
+}
---
+++ Source/IDDSwift/NumberFormatter.swift
@@ -0,0 +1,46 @@
+//
+//  NumberFormatter.swift
+//  IDDSwift
+//
+//  Created by Klajd Deda on 3/16/21.
+//  Copyright (C) 1997-2023 id-design, inc. All rights reserved.
+//
+
+import Foundation
+
+extension NumberFormatter {
+    public static let formaterWith3digits: NumberFormatter = {
+        let rv = NumberFormatter()
+        
+        rv.locale = Locale.init(identifier: "en_US_POSIX")
+        rv.maximumFractionDigits = 3
+        rv.minimumFractionDigits = 3
+        return rv
+    }()
+
+    public static let formaterWith2digits: NumberFormatter = {
+        let rv = NumberFormatter()
+
+        rv.locale = Locale.init(identifier: "en_US_POSIX")
+        rv.maximumFractionDigits = 2
+        rv.minimumFractionDigits = 2
+        return rv
+    }()
+}
+
+/**
+ Conveniences
+ let number1 = 0.123456
+ let cool1 = number1.with3Digits -> 0.123
+ let number2 = 120.123456
+ let cool2 = number2.with2Digits -> 120.12
+ */
+extension Double {
+    public var with3Digits: String {
+        return NumberFormatter.formaterWith3digits.string(from: self as NSNumber) ?? "0.196"
+    }
+
+    public var with2Digits: String {
+        return NumberFormatter.formaterWith2digits.string(from: self as NSNumber) ?? "0.19"
+    }
+}
