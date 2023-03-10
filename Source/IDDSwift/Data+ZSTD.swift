b59b880a1dee5d36bdce16ef2595956360572ba4
Klajd Deda <kdeda@id-design.com>
2023-03-10 07:01:29 -0500

    Ripping apart the ZSTDSwift for SWCompression

--- Source/IDDSwift/Data+ZSTD.swift
+++ Source/IDDSwift/Data+ZSTD.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Log4swift
+import SWCompression
 import ZSTDSwift
 
 public extension Data {
@@ -24,7 +25,7 @@ public extension Data {
             rv = try processor.compressBuffer(self, compressionLevel: 2)
         } catch ZSTDError.libraryError(let errStr) {
             Log4swift[Self.self].error("Library error: \(errStr)")
-        } catch ZSTDError.invalidCompressionLevel(let lvl){
+        } catch ZSTDError.invalidCompressionLevel(let lvl) {
             Log4swift[Self.self].error("Invalid compression level: \(lvl)")
         } catch ZSTDError.decompressedSizeUnknown {
             Log4swift[Self.self].error("Unknown decompressed size")
@@ -37,7 +38,7 @@ public extension Data {
         Log4swift[Self.self].info("in: '\(self.count.decimalFormatted)' out: '\(rv.count.decimalFormatted) bytes' in: '\(startDate.elapsedTime) ms'")
         return rv
     }
-    
+
     var zlibUncompressed: Data {
         let startDate = Date.init()
         var rv = Data()
@@ -60,4 +61,41 @@ public extension Data {
         Log4swift[Self.self].info("in: '\(self.count.decimalFormatted)' out: '\(rv.count.decimalFormatted) bytes' in: '\(startDate.elapsedTime) ms'")
         return rv
     }
+
+    // Using pure swift, from: https://github.com/tsolomko/SWCompression
+    //
+    var zlibCompressedV2: Data {
+        let startDate = Date.init()
+        var rv = Data()
+
+        do {
+            rv = try GzipArchive.archive(data: self)
+        } catch {
+            Log4swift[Self.self].error("Unknown error.")
+        }
+
+        //let rv = self.compress(withAlgorithm: .lzfse) ?? Data()
+        //let rv = (self as NSData).zlibCompressed() as Data
+        Log4swift[Self.self].info("in: '\(self.count.decimalFormatted)' out: '\(rv.count.decimalFormatted) bytes' in: '\(startDate.elapsedTime) ms'")
+        return rv
+    }
+
+    // Using pure swift, from: https://github.com/tsolomko/SWCompression
+    //
+    var zlibUncompressedV2: Data {
+        let startDate = Date.init()
+        var rv = Data()
+
+        do {
+            rv = try GzipArchive.unarchive(archive: self)
+        } catch {
+            Log4swift[Self.self].error("Unknown error.")
+        }
+
+        //let rv = self.compress(withAlgorithm: .lzfse) ?? Data()
+        //let rv = (self as NSData).zlibCompressed() as Data
+        Log4swift[Self.self].info("in: '\(self.count.decimalFormatted)' out: '\(rv.count.decimalFormatted) bytes' in: '\(startDate.elapsedTime) ms'")
+        return rv
+    }
+
 }
