211c6639922c63395e45f209374a20e4baf02ae3
Klajd Deda <kdeda@id-design.com>
2023-04-08 14:36:00 -0400

    Pinned dependency packages.

--- Package.swift
+++ Package.swift
@@ -10,21 +10,20 @@ let package = Package(
         .macOS(.v11)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "IDDSwift",
             targets: ["IDDSwift"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/kdeda/idd-log4-swift-v2.git", from: "2.0.2"),
-        .package(url: "https://github.com/kdeda/idd-zstd-swift.git", from: "1.3.2"),
+        .package(url: "https://github.com/kdeda/idd-log4-swift.git", "2.0.1" ..< "3.0.0"),
+        .package(url: "https://github.com/kdeda/idd-zstd-swift.git", "2.0.1" ..< "3.0.0"),
         .package(url: "https://github.com/apple/swift-crypto.git", from: "2.2.4")
     ],
     targets: [
         .target(
             name: "IDDSwift",
             dependencies: [
-                .product(name: "Log4swift", package: "idd-log4-swift-v2"),
+                .product(name: "Log4swift", package: "idd-log4-swift"),
                 .product(name: "ZSTDSwift", package: "idd-zstd-swift"),
                 .product(name: "Crypto", package: "swift-crypto")
             ]
@@ -32,7 +31,7 @@ let package = Package(
         .testTarget(
             name: "IDDSwiftTests",
             dependencies: [
-                .product(name: "Log4swift", package: "idd-log4-swift-v2"),
+                .product(name: "Log4swift", package: "idd-log4-swift"),
                 .product(name: "ZSTDSwift", package: "idd-zstd-swift"),
                 .product(name: "Crypto", package: "swift-crypto")
             ]
