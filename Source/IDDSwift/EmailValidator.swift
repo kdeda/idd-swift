e2c27846768eee0e6565e25d1d314330cf6e86f1
Klajd Deda <kdeda@id-design.com>
2023-05-31 10:24:02 -0400

    Wiring the email validation for apple oses

--- Source/IDDSwift/EmailValidator.swift
+++ Source/IDDSwift/EmailValidator.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Log4swift
 
 /**
  valid emails are now _@foo.com
@@ -14,16 +15,28 @@ import Foundation
  Predicate strings and key-value coding are not supported in swift-corelibs-foundation
  */
 public struct EmailValidator {
-//    private static let __firstpart = "[A-Z0-9a-z_]([A-Z0-9a-z._%+-]{0,30}[A-Z0-9a-z])?"
-//    private static let __serverpart = "([A-Z0-9a-z]([A-Z0-9a-z-]{0,30}[A-Z0-9a-z])?\\.){1,5}"
-//    private static let __emailRegex = __firstpart + "@" + __serverpart + "[A-Za-z]{2,8}"
-//    private static let __emailPredicate = NSPredicate(format: "SELF MATCHES %@", __emailRegex)
+#if  os(iOS) || os(watchOS) || os(tvOS)
+    private static let __firstpart = "[A-Z0-9a-z_]([A-Z0-9a-z._%+-]{0,30}[A-Z0-9a-z])?"
+    private static let __serverpart = "([A-Z0-9a-z]([A-Z0-9a-z-]{0,30}[A-Z0-9a-z])?\\.){1,5}"
+    private static let __emailRegex = __firstpart + "@" + __serverpart + "[A-Za-z]{2,8}"
+    private static let __emailPredicate = NSPredicate(format: "SELF MATCHES %@", __emailRegex)
+#endif
 
+    /**
+     Validates an email address. Does not work on linux ...
+     */
     public static func isValid(emailAddress: String) -> Bool {
         if emailAddress.isEmpty {
             return false
         }
-//        return EmailValidator.__emailPredicate.evaluate(with: emailAddress)
-        return false
+
+        var isValid = true
+#if  os(iOS) || os(watchOS) || os(tvOS)
+        isValid = EmailValidator.__emailPredicate.evaluate(with: emailAddress)
+#endif
+        if !isValid {
+            Log4swift[Self.self].error("invalid: '\(emailAddress)'")
+        }
+        return isValid
     }
 }
