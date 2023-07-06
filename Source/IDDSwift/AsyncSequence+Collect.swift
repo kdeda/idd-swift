79e5695edce150a7c3f6f07aabfcb38d2a17b87d
Klajd Deda <kdeda@id-design.com>
2023-07-06 10:56:34 -0400

    Fixes, comments to AsyncSequence+Collect

--- Source/IDDSwift/AsyncSequence+Collect.swift
+++ Source/IDDSwift/AsyncSequence+Collect.swift
@@ -12,29 +12,38 @@ import Log4swift
 public extension AsyncSequence {
     /// Collect elements from an async sequence.
     ///
-    /// - Parameter waitForMilliseconds: The number of milliseconds to wait before emitting. By default
-    /// this is `1000` or one second.
+    /// - Parameter waitForMilliseconds: The number of milliseconds to wait before emitting.
+    /// By default this is `1000` or one second. The waitForMilliseconds must be a non zero
+    /// positive integer.
     /// - Returns: Returns: A new stream of array of all elements.
     func collect(waitForMilliseconds: Int = 0) -> AsyncStream<[Element]> {
         AsyncStream { continuation in
             let buffer = ArrayActor<Element>()
 
-            /// Receive data updates in this task
+            Log4swift[Self.self].info("waitForMilliseconds: '\(waitForMilliseconds) ms'")
+            // Receive data updates in this task
             let task1 = Task {
                 for try await element in self {
                     await buffer.append(element)
                 }
-                // once the real sequenc ends, terminate all
+
+                // once the real sequence ends
+                // emitt the last batch and terminate all
                 let batch = await buffer.popAll()
-                Log4swift[Self.self].info("batch: \(batch)")
+
+                // Log4swift[Self.self].info("batch.count: \(batch.count)")
                 if !batch.isEmpty {
                     continuation.yield(batch)
+
+                    // not really sure whay i had to inject a sleep here :-)
+                    // maybe because we are going to terminate this stream
+                    // and with it bother task1, task2 defined here
                     try? await Task.sleep(nanoseconds: NSEC_PER_MSEC * UInt64(50))
                 }
                 continuation.finish()
             }
             
-            /// re-emit upstream
+            // re-emit upstream
             let task2 = Task {
                 while !Task.isCancelled {
                     try? await Task.sleep(nanoseconds: NSEC_PER_MSEC * UInt64(waitForMilliseconds))
