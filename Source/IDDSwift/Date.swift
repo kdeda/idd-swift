25216e6bc880f595dc53e6d3dde13efd595f37ad
Klajd Deda <kdeda@id-design.com>
2025-04-29 02:18:28 -0400

    * Better formated elapsedTime

--- Source/IDDSwift/Date.swift
+++ Source/IDDSwift/Date.swift
@@ -78,19 +78,28 @@ public extension String {
 
 public extension Double {
     /**
-     For less than   10 ms add the 3 digits, ex: '9.325 ms'
-     For less than  100 ms add the 2 digits, ex: '99.32 ms'
-     For less than 1000 ms add the 3 digits, ex: '111.3 ms'
-     For more than 1000 ms decimal formated, ex: '1,001 ms'
+     For less than         `10` ms add the 3 digits,     ex: '9.32 msec'
+     For less than        `100` ms add the 2 digits,     ex: '99.3 msec'
+     For less than      `1_000` ms add the 3 digits,     ex: ' 999 msec'
+     For less than    ` 10_000` ms switch to sec,        ex: ' 9.99 sec'
+     For less than    `100_000` ms switch to sec,        ex: ' 99.9 sec'
+     For less than  `1_000_000` ms switch to sec,        ex: '  999 sec'
+     For less than `10_000_000` ms switch to sec,        ex: '9,999 sec'
      */
     var elapsedTime: String {
         if self < 10 {
-            return self.with3Digits + " ms"
+            return self.with2Digits + " msec"             // '9.32 msec'
         } else if self < 100 {
-            return self.with2Digits + " ms"
-        } else if self < 1000 {
-            return self.with1Digits + " ms"
+            return self.with1Digits + " msec"             // '99.3 msec'
+        } else if self < 1_000 {
+            return "\(Int(self))" + " msec"               // ' 999 msec'
+        } else if self < 10_000 {
+            return (self / 1000).with2Digits + " sec"     // ' 9.99 sec'
+        } else if self < 100_000 {
+            return (self / 1000).with1Digits + " sec"     // ' 99.9 sec'
+        } else if self < 1_000_000 {
+            return "\(Int(self / 1000))" + " sec"         // '  999 sec'
         }
-        return Int(self).decimalFormatted + " ms"
+        return Int(self / 1000).decimalFormatted + " sec" // '9,999 sec'
     }
 }
