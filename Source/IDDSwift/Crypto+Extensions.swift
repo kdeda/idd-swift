e2a004938b7e598cffcb92f8ee48bcb408d52b8b
Klajd Deda <kdeda@id-design.com>
2026-07-28 08:59:11 -0400

    * Refactored Crypto+Extensions.swift for clarity, composability and usability.

--- Source/IDDSwift/Crypto+Extensions.swift
+++ Source/IDDSwift/Crypto+Extensions.swift
@@ -1,5 +1,5 @@
 //
-//  URL+Crypto.swift
+//  Crypto+Extensions.swift
 //  IDDSwift
 //
 //  Created by Klajd Deda on 4/3/24.
@@ -11,13 +11,29 @@ import Log4swift
 import Crypto
 
 public extension Data {
+    func digestWithHasher<Hasher: HashFunction>(_ hasher: Hasher) -> Data {
+        var hasher_ = hasher
+        
+        hasher_.update(data: self)
+        return Data(hasher_.finalize())
+    }
+
+    /// Assumes self is a result of digestWithHasher
+    var digestString: String {
+        let tokens = self.map { String(format: "%02hhx", $0) }
+        return tokens.joined(separator: "")
+    }
+
+    var md5Digest: Data {
+        digestWithHasher(Insecure.MD5())
+    }
+
     /**
      returns a unique fingerprint
      ie: 2E79D73C-EAB5-44E0-9DEC-75602872402E
      */
     var md5: String {
-        let digest = Insecure.MD5.hash(data: self)
-        var tokens = digest.map { String(format: "%02hx", $0) }
+        var tokens = md5Digest.map { String(format: "%02hhx", $0) }
 
         if tokens.count == 16 {
             tokens.insert("-", at: 4)
@@ -31,6 +47,22 @@ public extension Data {
             //  }
         }
         return tokens.joined(separator: "").uppercased()
+    }
+
+    var sha1Digest: Data {
+        digestWithHasher(Insecure.SHA1())
+    }
+    
+    var sha1: String {
+        sha1Digest.digestString
+    }
+
+    var sha256Digest: Data {
+        digestWithHasher(SHA256())
+    }
+
+    var sha256: String {
+        sha256Digest.digestString
     }
 }
 
@@ -96,19 +128,12 @@ public extension URL {
      Use it as such
      ```
      let url = URL() // assume this is readable
-     let digest = try url.calculateHash(SHA256())
+     let digest = try url.digestFromHasher(SHA256())
      let sha256 = digest.compactMap { String(format: "%02x", $0) }.joined()
      ```
      Co-operates in the Task cancelation, so it can early exit on large files.
      */
-    func calculateHash<Hasher: HashFunction>(_ hasher: Hasher) throws -> Data {
-        //        guard self.isReadable
-        //        else {
-        //            let fileExist = self.fileExist
-        //
-        //            Log4swift[Self.self].error("fileExist: '\(fileExist)' filePath: '\(self.path)'")
-        //            throw CalculateHashError.emptyFile
-        //        }
+    func digestWithHasher<Hasher: HashFunction>(_ hasher: Hasher) throws -> Data {
         let handle = try FileHandle(forReadingFrom: self)
         defer {
             handle.closeFile()
@@ -147,31 +172,35 @@ public extension URL {
     /**
      Slow on apple silicon as of Xcode26
      */
-    var md5: String? {
+    var md5Digest: Data? {
         let startDate = Date()
-        let rv: String? = {
+        let rv: Data? = {
             do {
-                return try calculateHash(Insecure.MD5()).md5
+                return try digestWithHasher(Insecure.MD5())
             } catch {
                 Log4swift[Self.self].error("error: '\(error)'")
                 return .none
             }
         }()
 
-        if startDate.elapsedTimeInMilliseconds > 100 {
-            Log4swift[Self.self].info("url: '\(self.path)' md5: '\(rv)' from: '\(logicalSize.decimalFormatted) bytes' elapsedTime: '\(startDate.elapsedTime)'")
+        if let rv, startDate.elapsedTimeInMilliseconds > 100 {
+            Log4swift[Self.self].info("url: '\(self.path)' with: '\(logicalSize.decimalFormatted) bytes' completed in: '\(startDate.elapsedTime)'")
         }
         return rv
+    }
+    
+    var md5: String? {
+        md5Digest?.md5
     }
 
     /**
      A lot faster than the md5, like 3x on apple silicon
      */
-    var sha1: String? {
+    var sha1Digest: Data? {
         let startDate = Date()
-        let rv: String? = {
+        let rv: Data? = {
             do {
-                return try calculateHash(Insecure.SHA1()).md5
+                return try digestWithHasher(Insecure.SHA1())
             } catch {
                 Log4swift[Self.self].error("error: '\(error)'")
                 return .none
@@ -179,92 +208,36 @@ public extension URL {
         }()
 
         if startDate.elapsedTimeInMilliseconds > 100 {
-            Log4swift[Self.self].info("url: '\(self.path)' sha1: '\(rv ?? "unknown")' from: '\(logicalSize.decimalFormatted) bytes' elapsedTime: '\(startDate.elapsedTime)'")
+            Log4swift[Self.self].info("url: '\(self.path)' with: '\(logicalSize.decimalFormatted) bytes' completed in: '\(startDate.elapsedTime)'")
         }
         return rv
+    }
+    
+    var sha1: String? {
+        sha1Digest?.digestString
     }
 
     /**
      A lot faster than the md5, like 3x on m2 ultra
      */
-    var sha256: String? {
+    var sha256Digest: Data? {
         let startDate = Date()
-        let rv: String? = {
+        let rv: Data? = {
             do {
-                return try calculateHash(SHA256()).md5
+                return try digestWithHasher(SHA256())
             } catch {
                 Log4swift[Self.self].error("error: '\(error)'")
                 return .none
             }
         }()
 
-        if startDate.elapsedTimeInMilliseconds > 1_000 {
-            Log4swift[Self.self].info("url: '\(self.path)' sha256: '\(rv)' from: '\(logicalSize.decimalFormatted) bytes' elapsedTime: '\(startDate.elapsedTime)'")
+        if let rv, startDate.elapsedTimeInMilliseconds > 1_000 {
+            Log4swift[Self.self].info("url: '\(self.path)' with: '\(logicalSize.decimalFormatted) bytes' completed in: '\(startDate.elapsedTime)'")
         }
         return rv
     }
-}
-
-/**
- Deprecated, not used
- */
-public extension URL {
-    var md5_deprecated: String {
-        guard let handle = try? FileHandle(forReadingFrom: self)
-        else { return "" }
-        defer {
-            handle.closeFile()
-        }
-        let logicalSize = self.logicalSize
-        guard logicalSize > 0
-        else { return "" }
-
-        let startDate = Date()
-        var hasher = Insecure.MD5()
-
-        while let nextChunk = handle.readData(FileHandle.bufferLength) {
-            hasher.update(data: nextChunk)
-        }
-
-        guard !Task.isCancelled // preemptive cancellation
-        else { return "" }
-        let data = Data(hasher.finalize())
-        let rv = data.md5
-
-        if startDate.elapsedTimeInMilliseconds > 100 {
-            Log4swift[Self.self].info("url: '\(self.path)' md5: '\(rv)' from: '\(logicalSize.decimalFormatted) bytes' elapsedTime: '\(startDate.elapsedTime)'")
-        }
-        return rv
-    }
-
-    /**
-     A lot faster than the md5, like 4x on m2 ultra
-     */
-    var sha256_deprecated: String {
-        guard let handle = try? FileHandle(forReadingFrom: self)
-        else { return "" }
-        defer {
-            handle.closeFile()
-        }
-        let logicalSize = self.logicalSize
-        guard logicalSize > 0
-        else { return "" }
-
-        let startDate = Date()
-        var hasher = SHA256()
-
-        while let nextChunk = handle.readData(FileHandle.bufferLength) {
-            hasher.update(data: nextChunk)
-        }
-
-        guard !Task.isCancelled // preemptive cancellation
-        else { return "" }
-        let data = Data(hasher.finalize())
-        let rv = data.md5
-
-        if startDate.elapsedTimeInMilliseconds > 100 {
-            Log4swift[Self.self].info("url: '\(self.path)' sha256: '\(rv)' from: '\(logicalSize.decimalFormatted) bytes' elapsedTime: '\(startDate.elapsedTime)'")
-        }
-        return rv
+    
+    var sha256: String? {
+        sha256Digest?.digestString
     }
 }
